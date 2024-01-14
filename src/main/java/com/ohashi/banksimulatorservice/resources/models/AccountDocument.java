9fe617269c46ed6089843874dbdb5ae3bdf51022
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-01-14 20:10:49 -0300

    feat: add inner class to builder strategy

--- src/main/java/com/ohashi/banksimulatorservice/resources/models/AccountDocument.java
+++ src/main/java/com/ohashi/banksimulatorservice/resources/models/AccountDocument.java
@@ -22,14 +22,108 @@ public class AccountDocument {
         this.status = status;
     }
 
+    private AccountDocument(Builder builder) {
+        this.agencyNumber = builder.agencyNumber;
+        this.accountNumber = builder.accountNumber;
+        this.name = builder.name;
+        this.lastName = builder.lastName;
+        this.email = builder.email;
+        this.phone = builder.phone;
+        this.status = builder.status;
+    }
+
     @Id
     @Indexed
     private ObjectId _id;
     private int agencyNumber;
+
+    @Indexed
     private int accountNumber;
     private String name;
     private String lastName;
-    private String email;
-    private String phone;
     private AccountStatus status;
+
+    @Indexed
+    private String email;
+
+    @Indexed
+    private String phone;
+
+    public int getAgencyNumber() {
+        return agencyNumber;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public static class Builder {
+
+        private int agencyNumber;
+        private int accountNumber;
+        private String name;
+        private String lastName;
+        private String email;
+        private String phone;
+        private AccountStatus status;
+
+        public AccountDocument build() {
+            return new AccountDocument(this);
+        }
+
+        public Builder setAgencyNumber(int agencyNumber) {
+            this.agencyNumber = agencyNumber;
+            return this;
+        }
+
+        public Builder setAccountNumber(int accountNumber) {
+            this.accountNumber = accountNumber;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setStatus(AccountStatus status) {
+            this.status = status;
+            return this;
+        }
+    }
 }
