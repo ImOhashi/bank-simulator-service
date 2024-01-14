aaf79c52f6ca51756a7b2de96e8654309b630871
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-01-14 19:16:35 -0300

    first commit

---
+++ src/main/java/com/ohashi/banksimulatorservice/BankSimulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ohashi.banksimulatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSimulatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSimulatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohashi/banksimulatorservice/BankSimulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.banksimulatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSimulatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
